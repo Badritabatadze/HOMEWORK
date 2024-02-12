485ea7098a1cbfdb37dc2e8cc26b5b630d77a8cf
Badritabatadze <tabatadzebadri39@gmail.com>
2024-02-12 20:06:14 +0400

    Badritabatadze

--- Day 02/Homework/homework.py
+++ Day 02/Homework/homework.py
@@ -1,7 +1,6 @@
-from turtle import *
 
+print("im looking forward to become CHAD i loved the first lesson im making friends on the way like datigelashvili; vache gu; nikoloz kimeridze i have hope il get closer to other goa's member")
 
-print im looking forward to become CHAD i loved the first lesson im making friends on the way like datigelashvili; vache gu; nikoloz kimeridze i have hope il get closer to other goa's member
 
 
 
