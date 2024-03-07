91f304d40b09c9323724e2aa6a28a22cf6cd57d6
Badritabatadze <tabatadzebadri39@gmail.com>
2024-03-07 14:43:29 +0400

    Badritabatadze

---
+++ Day 05/homework/homework.py
@@ -0,0 +1,20 @@
+
+name = "badri"
+print( type(name))
+
+score = 100
+
+print(type (score))
+
+devision = 100/3
+
+print( type(devision))
+
+
+
+print(200/7)
+print(213/435)
+print(34*54)
+print(5467*546)
+print(345-768)
+print(943587+645879)
