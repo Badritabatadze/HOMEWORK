cb7b14ab50a85f632b5c31857ff9e8a30500ac5b
Badritabatadze <tabatadzebadri39@gmail.com>
2024-03-10 19:51:27 +0400

    Badritabatadze

---
+++ Day 06/homework/homework.py
@@ -0,0 +1,83 @@
+print(4257>67 or 342>345)
+print(4378>4598 or 65>5647)
+print(82463>564 or 5478<5674)
+print(42>1 or 6758<64)
+print(1323<0 or 56<33)
+
+
+print(4>67 and 546<34)
+print(48>45798 and 45)
+print(82387463>564 and 90>90)
+print(4>1 and 345<789)
+print(133<0 and 4322<43)
+
+
+print(4>5 and 57<4) #false
+print(5>9 or 4>0) #true
+
+
+
+num1 = input("pls enter first num :")
+num2 = input("pls enter second num :")
+
+if num1>num2 :
+    print("first number is higher")
+elif num1<num2 :
+    print("second number is higher")
+elif num1 == num2 :
+    print("first number = second number")
+
+
+
+
+
+
+
+variable = 432>78
+variable = 34<322
+print(432>78 or 34<322)
+
+
+
+surname = "badritabatadze"
+print( type(surname))
+
+score = 100
+
+print(type (score))
+
+number = 100/3
+
+print( type(number))
+
+familymember = "father"
+print(type(familymember))
+
+
+
+name = input("pls classify ur identity :")
+group = input("pls enter ur assigned group number : ")
+print("hello " +  name  +  " "  "Welcome to Group " + group)
+
+
+
+print(3425/6589)
+print(type(3425/6589))
+
+
+num1 = 20
+num2 = 24
+year = num1 * 100 + num2
+print( year )
+
+float1 = 3.14
+float2 = 2.718
+float3 = 1.618
+float4 = 0.577
+float5 = 1.732
+
+print(int(float1))
+print(int(float2))
+print(int(float3))
+print(int(float4))
+print(int(float5))
---
+++ Day 06/homework/test.py
@@ -0,0 +1,9 @@
+num1 = input("pls enter first num :")
+num2 = input("pls enter second num :")
+
+if num1>num2 :
+    print("first number is higher")
+elif num1<num2 :
+    print("second number is higher")
+elif num1 == num2 :
+    print("first number = second number")
