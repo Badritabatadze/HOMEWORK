dc82f71411fe494be615dbcdab1d9d5b78b6d596
Badritabatadze <tabatadzebadri39@gmail.com>
2024-03-18 21:15:19 +0400

    Badritabatadze

---
+++ Day 07/homework/homework.py
@@ -0,0 +1,31 @@
+ user_1 = input("Enter a string: ")
+ user_2 = int(input("Enter an integer: "))
+ user_3 = float(input("Enter a float: "))
+ user_4 = bool(input("Enter a boolean (True or False): "))
+
+    
+
+
+
+print(47>67 or 3402>345)
+print(478>4598 or 65>5)
+print(863>43 or 578<5674)
+print(42>1 or 6758<64)
+print(1323>0 or 56<33)
+
+
+
+
+
+print(True and True )#true
+print(True and False )#false
+print(False and True )#false
+print(False and False )#false
+
+
+
+
+print(True or True ) #true
+print(True or False )#true
+print(False or True )#true
+print(False or False )#false
